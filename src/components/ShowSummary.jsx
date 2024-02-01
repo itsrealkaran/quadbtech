be5fb81e8fae32a6bbb9d7467380c6bd95cdb19e
itsrealkaran <karansingh@duck.com>
2024-02-01 08:18:40 +0530

    Bug Fixes

--- src/components/ShowSummary.jsx
+++ src/components/ShowSummary.jsx
@@ -1,7 +1,7 @@
 function ShowSummary({ show, goBackToList }) {
     return (
       <div className="show-summary">
-        <img src={show.show.image.original} alt={show.show.name} />
+        <img src={show.show.image?.original || "https://via.placeholder.com/210x280"} alt={show.show.name} />
         <div className="show-summary-content">
           <h1>{show.show.name}</h1>
           <p dangerouslySetInnerHTML={{ __html: show.show.summary }} />
